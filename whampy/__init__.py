ff0afb06e22990e22542d3b7967df449e53a78da
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2026-02-14 16:31:16 +0100

    removed polymc

--- whampy/__init__.py
+++ whampy/__init__.py
@@ -1,1 +1,12 @@
+"""
+WHAMpy
+=================
+
+"""
+
+__version__ = "0.1.0"
+__author__ = "Enrico Skoruppa"
+
 from .wham_solver import WhamSolver, WhamResult
+
+__all__ = ["WhamSolver", "WhamResult"]
